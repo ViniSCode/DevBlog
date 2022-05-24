48125f64ed7df68ee93746259121f14c754fcb42
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-05-24 12:05:29 -0300

    comment section

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,7 +1,10 @@
 import { RichText } from 'prismic-dom';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { database } from '../../services/firebase';
+import { Button } from './../../components/Button/index';
 import { Loading } from './../../components/Loading/index';
+import { useAuth } from './../../hooks/useAuth';
 import { getPrismicClient } from './../../services/prismic';
 import styles from './post.module.scss';
 
@@ -9,15 +12,15 @@ export function Post () {
   const params = useParams();
   const slug = params.id;
   const [post, setPost] = useState();
-
+  const { user, handleSignInWithGoogle} = useAuth();
+  const [newComment, setNewComment] = useState();
+  const databaseSlug = slug.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  
   //get post by UID - slug
   useEffect(() => {
-
     const getPostsFromPrismic  = async () => {
       const client = await getPrismicClient();
-      
       const response = await client.getByUID('post', String(slug), {});
-      console.log(response);
       const formattedPosts = {
         slug,
         image:response.data?.image.url,
@@ -37,22 +40,73 @@ export function Post () {
 
     getPostsFromPrismic()
     
-  }, [])
-  
+  }, []);
+
+  async function handleSendComment (event) {
+    event.preventDefault();
+
+    if (newComment.trim() === "") {
+      return;
+    }
+
+    if (!user) {
+      alert("You must be logged in to send a comment");
+      throw new Error("You must be logged in");
+    }
+    
+    const comment = {
+      content: newComment,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+    }
+    await database.ref(`posts/${databaseSlug}/comments`).push(comment);
+    setNewComment('');
+  }
+
   return (
     <div className={styles.container}>
-      {!post ? <Loading/> : 
+      {!post ? <Loading/>: 
       (
-        <article className={styles.post}>
-          <h1>{post.title}</h1>
-          <time>{post.updatedAt}</time>
-          {post.image && <img src={post.image} />}
-          <div 
+        <>
+          <article className={styles.post}>
+            <h1>{post.title}</h1>
+            <time>{post.updatedAt}</time>
+            {post.image && <img src={post.image} />}
+            <div 
             className={styles.postContent}
             dangerouslySetInnerHTML={{ __html: post.content }} 
           />
-        </article>
+          </article>
+        </>
       )}
+
+      <div className="addComment">
+      <form onSubmit={handleSendComment}>
+          <textarea 
+            placeholder="Comment something here..."
+            onChange={event => setNewComment(event.target.value)}
+            value={newComment}
+          />
+
+          <div className={styles.formFooter}>
+            { user ? (
+              <>
+                <div className={styles.userInfo}>
+                  <img src={user?.avatar} alt={user.name}/>
+                  <span>{user.name}</span>
+                </div>
+                <Button type="submit">Comment</Button>
+              </>
+            ) : (<span>Para enviar uma pergunta, <button type="button" onClick={handleSignInWithGoogle}>faça seu login</button>.</span>)}
+          </div>
+        </form>
+
+        <div className="comments">
+            {/* <Comments slug={slug}/> */}
+          </div>
+      </div>
     </div>
   )
 }
