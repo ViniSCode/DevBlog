42199ae809e7232aa4ee8231f8ec8ac39a55b139
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-05-30 16:20:13 -0300

    responsiveness

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,78 @@
+import { useState } from 'react';
+import { AiOutlineLogin } from 'react-icons/ai';
+import { BiHome, BiNews } from 'react-icons/bi';
+import { FiLogOut } from 'react-icons/fi';
+import { HiMenuAlt1 } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import { useAuth } from '../../hooks/useAuth';
-import styles from './styles.module.scss';
+import './styles.scss';
 export function Header () {
   const {user, handleSignInWithGoogle} = useAuth();
-
   const {pathname} = useLocation();  
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
  return ( 
-  <header className={styles.header}>
-      <nav className={styles.nav}>
-        <div>
+  <header className="header">
+      <nav className="nav">
+        <div className="logoImg">
           <img src={logoImg} alt="devblog" />
         </div>
-        <ul className={styles.ul}>
-          <li><Link to="/" className={pathname === "/" ? styles.active : ''}>
-            Home
-          </Link></li>
+        
+        <div className={"menu " + (isMenuActive ? "active" : "")}>
+          
+          <ul className={"ul " + (isMenuActive ? "active" : "")}>
+            <li className="li" onClick={() => setIsMenuActive(false)}>
+              <Link to="/" className={pathname === "/" ? "active" : ''}>
+              {isMenuActive && <BiHome className='homeIcon'/>}
+              Home
+            </Link></li>
 
-          <li><Link to="/posts" className={pathname === "/posts" ? styles.active : ''}>
-            Posts
-          </Link></li>
-          { 
-            user ? 
-            <button 
-              className={styles.username}
-            >
-              {user.name}
-            </button> :
+            <li className="li" onClick={ () => setIsMenuActive(false)}>
+              <Link to="/posts" className={pathname === "/posts" ? "active" : ''}>
+              {isMenuActive && <BiNews className='newsIcon'/>}
+              Posts
+            </Link></li>
             
-            <button onClick={handleSignInWithGoogle} className={styles.loginBtn}>
-              Login 
-            </button>
-          }         
-        </ul>
+            { 
+              user ? (
+                !isMenuActive ? (
+                  <button 
+                    className="username"
+                  >
+                    {user.name}
+                  </button>   
+                ) :
+                (
+                    <button 
+                      className="logOut"
+                    >
+                      <FiLogOut className="logoutIcon"/>
+                      Logout
+                  </button>   
+                )
+              )
+              :
+              (
+                isMenuActive ? (
+                  <button onClick={handleSignInWithGoogle} className="loginBtn">
+                    <AiOutlineLogin className="loginIcon"/>
+                    Login 
+                  </button>
+                ) : 
+                (
+                  <button onClick={handleSignInWithGoogle} className="loginBtn">
+                    Login 
+                  </button>
+                )
+              )
+            }         
+          </ul>
+        </div>
+
+        <div className="mobileMenuContainer">
+            <HiMenuAlt1 className='menuIcon' onClick={() => setIsMenuActive(!isMenuActive)}/>
+        </div>
       </nav>
     </header>
   );
