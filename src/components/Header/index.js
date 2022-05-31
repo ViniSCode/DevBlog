e4123c12fcf026e91670fe5212142fb89e8a380e
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-05-31 15:47:53 -0300

    finalizing app responsiveness

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import { AiOutlineLogin } from 'react-icons/ai';
 import { BiHome, BiNews } from 'react-icons/bi';
 import { FiLogOut } from 'react-icons/fi';
 import { HiMenuAlt1 } from 'react-icons/hi';
+import { IoClose } from 'react-icons/io5';
 import { Link, useLocation } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import { useAuth } from '../../hooks/useAuth';
@@ -12,23 +13,35 @@ export function Header () {
   const {pathname} = useLocation();  
   const [isMenuActive, setIsMenuActive] = useState(false);
 
+  function handleOpenMenu() {
+    setIsMenuActive(!isMenuActive);
+    document.body.style.overflowY = "hidden";
+  }
+
+  function handleCloseMenu () {
+    setIsMenuActive(false);
+    document.body.style.overflowY = "auto";
+  }
+
  return ( 
   <header className="header">
       <nav className="nav">
         <div className="logoImg">
-          <img src={logoImg} alt="devblog" />
+          <Link to="/">
+            <img src={logoImg} alt="devblog" />
+          </Link>
         </div>
         
         <div className={"menu " + (isMenuActive ? "active" : "")}>
           
           <ul className={"ul " + (isMenuActive ? "active" : "")}>
-            <li className="li" onClick={() => setIsMenuActive(false)}>
+            <li className="li" onClick={handleCloseMenu}>
               <Link to="/" className={pathname === "/" ? "active" : ''}>
               {isMenuActive && <BiHome className='homeIcon'/>}
               Home
             </Link></li>
 
-            <li className="li" onClick={ () => setIsMenuActive(false)}>
+            <li className="li" onClick={handleCloseMenu}>
               <Link to="/posts" className={pathname === "/posts" ? "active" : ''}>
               {isMenuActive && <BiNews className='newsIcon'/>}
               Posts
@@ -71,7 +84,11 @@ export function Header () {
         </div>
 
         <div className="mobileMenuContainer">
-            <HiMenuAlt1 className='menuIcon' onClick={() => setIsMenuActive(!isMenuActive)}/>
+          <HiMenuAlt1 className={"menuIcon " + (isMenuActive ? "active" : "")} onClick={handleOpenMenu}/> 
+          { 
+             isMenuActive &&
+            <IoClose className={"closeMenuIcon " + (isMenuActive ? "active" : "")}  onClick={handleCloseMenu}/> 
+          } 
         </div>
       </nav>
     </header>
